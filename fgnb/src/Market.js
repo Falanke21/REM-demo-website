8a23e8cdd501be078d138e63abfe454f7a5c36f3
dinoli <dino.li@mail.utoronto.ca>
2019-10-23 23:44:05 -0400

    Add a page of Market and a folder stores for the resource

--- fgnb/src/Market.js
+++ fgnb/src/Market.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+import './Market.css';
 class Market extends Component {
-  render() {
-    return (
-      <div> I'm the market page !!!!!!!!!!!!!!</div>
-    );
-  }
+
+	render(){
+		return (<div className = "product-card"> 
+					<h1> Here is something for sale </h1>
+					<p> here is the description </p>
+					<div className = "product-pic"></div>
+					<div className = "product-info">
+						<div className = "product-price"> $90</div>
+						<button className = "buy-button" id = "buy-button"> Click To Buy </button>
+					</div>
+
+			</div>
+
+			);
+	}
 }
 
 export default Market;
