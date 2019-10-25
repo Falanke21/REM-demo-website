fc9ca40f2675b3e5af3c0212efc9fd95e69946b7
chriszhang <chris19980401@gmail.com>
2019-10-24 23:25:58 -0400

    AuthForm added

--- fgnb/src/AuthForm.js
+++ fgnb/src/AuthForm.js
@@ -3,13 +3,14 @@ import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import Checkbox from '@material-ui/core/Checkbox';
 import { makeStyles, emphasize } from '@material-ui/core/styles';
+import { Link, Redirect } from "react-router-dom";
 import './AuthForm.css'
 
 const useStyles = makeStyles({
   button: {
     float: "right",
     position: "relative",
-    background: 'grey',
+    background: 'pink',
     border: 0,
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
@@ -20,7 +21,7 @@ const useStyles = makeStyles({
   }
 })
 
-function Hook() {
+function SignUpButton() {
   const classes = useStyles();
   return <Button className={classes.button}>SignUp</Button>;
 }
@@ -79,7 +80,9 @@ function Hook() {
           <tr className="signUptext">
           If you do not have an account yet, click here!
           </tr>
-           <Hook></Hook>
+          <Link to={"./signup"}>
+          <SignUpButton></SignUpButton>
+          </Link>
           </p>
 
         </div>
