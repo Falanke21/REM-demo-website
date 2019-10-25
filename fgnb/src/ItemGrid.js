83239bc34cdf51f06e1e3edd2ce46cd8209640fb
Falanke21 <1065314944@qq.com>
2019-10-24 23:03:32 -0400

    add some flexbox into itemGrid

---
+++ fgnb/src/ItemGrid.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './ItemGrid.css';
+
+class ItemGrid extends Component {
+  render() {
+    return (
+      <div className="grid-container"> 
+		<div className="item">Ass We Can</div>
+		<div className="item">Ass We Can</div>
+		<div className="item">Ass We Can</div>
+	</div>
+    );
+  }
+}
+
+export default ItemGrid;
