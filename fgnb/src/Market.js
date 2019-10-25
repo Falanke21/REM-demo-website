795921bc1fed097dba57e13622a1a9f3ac1eb0cf
Falanke21 <1065314944@qq.com>
2019-10-24 20:35:49 -0400

    some initial design on Market with Navigation bar

--- fgnb/src/Market.js
+++ fgnb/src/Market.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Container from "@material-ui/core/Container";
 
-import './Market.css';
+import Navigation from './Navigation';
+
+import "./Market.css";
 class Market extends Component {
-
-	render(){
-		return (<div className = "product-card"> 
-					<h1> Here is something for sale </h1>
-					<p> here is the description </p>
-					<div className = "product-pic"></div>
-					<div className = "product-info">
-						<div className = "product-price"> $90</div>
-						<button className = "buy-button" id = "buy-button"> Click To Buy </button>
-					</div>
-
-			</div>
-
-			);
-	}
+  render() {
+    return (
+      <div>
+        <Navigation />
+          <span> Here is something for sale </span>
+      </div>
+    );
+  }
 }
 
 export default Market;
