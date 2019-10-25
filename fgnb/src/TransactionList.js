508229b5abff209404e95d81d05fa1e3e1931c08
zuoqiwx <zuoqiwchris@gmail.com>
2019-10-24 21:18:35 -0400

    Implemented add transaction functionality

--- fgnb/src/TransactionList.js
+++ fgnb/src/TransactionList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, TableHead, TableBody, TableRow, TableCell, IconButton } from '@material-ui/core';
+import { Grid, Table, TableHead, TableBody, TableRow, TableCell, IconButton, Icon, Input } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 import Transaction from './Transaction.js';
@@ -12,6 +12,9 @@ class TransactionList extends React.Component {
   constructor() {
     super();
     this.state = {
+      id: "",
+      from: "",
+      to: "",
       data: [
         createTranx(0, "user1", "user2"),
         createTranx(1, "user2", "user1")
@@ -20,7 +23,17 @@ class TransactionList extends React.Component {
   }
 
   addTransaction = () => {
-
+    let transactions = this.state.data;
+    if (transactions.filter((t) => {
+      return t.id === this.state.id;
+    }).length > 0) {
+      alert("Invalid ID");
+    } else {
+      transactions.push(createTranx(this.state.id, this.state.from, this.state.to));
+      this.setState({
+        data: transactions
+      });
+    }
   };
 
   updateTransaction = (transaction, id, from, to) => {
@@ -49,33 +62,81 @@ class TransactionList extends React.Component {
     });
   };
 
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     const transactionList = this.state.data;
     return (
-      <Table size='small'>
-        <TableHead>
-          <TableRow>
-            <TableCell align='center'>Actions</TableCell>
-            <TableCell align='right'>ID</TableCell>
-            <TableCell align='right'>From</TableCell>
-            <TableCell align='right'>To</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            transactionList.map((transaction) => {
-              return (
-                <Transaction 
-                  key={transaction.id}
-                  transaction={transaction}
-                  updateTransaction={this.updateTransaction}
-                  removeTransaction={this.removeTransaction}
-                />
-              );
-            })
-          }
-        </TableBody>
-      </Table>
+      <div>
+        <Grid
+          container
+          direction='row'
+          justify='space-evenly'
+          alignItems='center'
+        >
+          <Grid item xs={1}>
+            <IconButton onClick={this.addTransaction}>
+              <AddIcon/>
+            </IconButton>
+          </Grid>
+          <Grid item xs={3}>
+            <Input
+              name='id'
+              value={this.state.id}
+              onChange={this.handleChange}
+              type='number'
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Input
+              name='from'
+              value={this.state.from}
+              onChange={this.handleChange}
+              type='text'
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Input
+              name='to'
+              value={this.state.to}
+              onChange={this.handleChange}
+              type='text'
+            />
+          </Grid>
+        </Grid>
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell align='center'>Actions</TableCell>
+              <TableCell align='right'>ID</TableCell>
+              <TableCell align='right'>From</TableCell>
+              <TableCell align='right'>To</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              transactionList.map((transaction) => {
+                return (
+                  <Transaction 
+                    key={transaction.id}
+                    transaction={transaction}
+                    updateTransaction={this.updateTransaction}
+                    removeTransaction={this.removeTransaction}
+                  />
+                );
+              })
+            }
+          </TableBody>
+        </Table>
+      </div>
     );
   }
 }
