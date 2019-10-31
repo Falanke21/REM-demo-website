00ff850b5cb573721a2c1dfb4c35efac87e1dae6
zuoqiwx <zuoqiwchris@gmail.com>
2019-10-31 19:43:13 -0400

    Added top navigation bar and side drawer to admin dashboard

--- fgnb/src/AdminDashBoard.js
+++ fgnb/src/AdminDashBoard.js
@@ -1,31 +1,89 @@
-import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
+import React from "react";
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Drawer,
+    Paper,
+    Typography,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
-import UserPanel from './UserPanel.js';
-import TransactionList from './TransactionList';
+import UserPanel from "./UserPanel.js";
+import TransactionList from "./TransactionList";
 
 class AdminDashBoard extends React.Component {
-  render() {
-    return (
-      <Grid
-        container
-        direction='row'
-        justify='space-evenly'
-        alignItems='flex-start'
-      >
-        <Grid item xs={3}>
-          <Paper>
-            <UserPanel/>
-          </Paper>
-        </Grid>
-          <Grid item xs={7}>
-            <Paper>
-              <TransactionList/>
-            </Paper>
-          </Grid>
-      </Grid>
-    );
-  }
+    state = {
+        sideList: false,
+        currPage: "User"
+    };
+
+    toggleDrawer = open => event => {
+        this.setState({
+            sideList: open
+        });
+    };
+
+    moveView = newView => event => {
+        this.setState({
+            currPage: newView
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            onClick={this.toggleDrawer(true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography>Dashboard</Typography>
+                    </Toolbar>
+                </AppBar>
+                <Toolbar />
+                <Drawer
+                    open={this.state.sideList}
+                    onClose={this.toggleDrawer(false)}
+                    moveView={this.moveView}
+                >
+                    <div
+                        style={{ width: 300 }}
+                        onClick={this.toggleDrawer(false)}
+                    >
+                        <List>
+                            {["User", "Transaction"].map(item => (
+                                <ListItem button onClick={this.moveView(item)}>
+                                    <ListItemIcon>
+                                        <MenuIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={item} />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Drawer>
+                {this.state.currPage === "User" && (
+                    <Paper>
+                        <UserPanel />
+                    </Paper>
+                )}
+                {this.state.currPage === "Transaction" && (
+                    <Paper>
+                        <TransactionList />
+                    </Paper>
+                )}
+            </div>
+        );
+    }
 }
 
 export default AdminDashBoard;
