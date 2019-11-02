23dd3fb28689126f1a42de1e62b9c6915bfa9777
Falanke21 <1065314944@qq.com>
2019-11-01 22:52:07 -0400

    add fake ids to tile data

--- fgnb/src/tileData.js
+++ fgnb/src/tileData.js
@@ -1,5 +1,6 @@
 const tileData = [
     {
+      id: "0",
       img: require('./asset/breakfast.jpg'),
       title: 'Breakfast',
       author: 'jill111',
@@ -7,60 +8,70 @@ const tileData = [
       featured: true,
     },
     {
-  
+      id: "1",
       img: require('./asset/burgers.jpg'),
       title: 'Tasty burger',
       author: 'director90',
     },
     {
+      id: "2",
       img: require('./asset/camera.jpg'),
       title: 'Camera',
       author: 'Danson67',
     },
     {
+      id: "3",
       img: require('./asset/morning.jpg'),
       title: 'Morning',
       author: 'fancycrave1',
       featured: true,
     },
     {
+      id: "4",
       img: require('./asset/hats.jpg'),
       title: 'Hats',
       author: 'Hans',
     },
     {
+      id: "5",
       img: require('./asset/honey.jpg'),
       title: 'Honey',
       author: 'fancycravel',
     },
     {
+      id: "6",
       img: require('./asset/vegetables.jpg'),
       title: 'Vegetables',
       author: 'jill111',
       cols: 2,
     },
     {
+      id: "7",
       img: require('./asset/plant.jpg'),
       title: 'Water plant',
       author: 'BkrmadtyaKarki',
     },
     {
+      id: "8",
       img: require('./asset/mushroom.jpg'),
       title: 'Mushrooms',
       author: 'PublicDomainPictures',
     },
     {
+      id: "9",
       img: require('./asset/olive.jpg'),
       title: 'Olive oil',
       author: 'congerdesign',
     },
     {
+      id: "10",
       img: require('./asset/star.jpg'),
       title: 'Sea star',
       cols: 2,
       author: '821292',
     },
     {
+      id: "11",
       img:require('./asset/bike.jpg'),
       title: 'Bike',
       author: 'danfador',
