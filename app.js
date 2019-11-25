c291c9b4d8eb608ba57af40b20ec24b6fa4fecca
Falanke21 <1065314944@qq.com>
2019-11-25 11:55:44 -0500

    add front-end index.html routes, make sure working

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var exampleRouter = require('./routes/example');
-const signupRouter = require('./routes/signup');
+var exampleRouter = require("./routes/example");
+const signupRouter = require("./routes/signup");
 
 // mongoose and mongo connection
-const { mongoose } = require('./db/mongoose')
+const { mongoose } = require("./db/mongoose");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // EXAMPLES
-app.use('/', indexRouter);
-app.use('/example', exampleRouter);
+app.use("/example", exampleRouter);
+
+/*
+    ALL JSON APIs goes to routes directory
+    ALL HTML file routes stay in this file
+*/
 
 // REAL APIs
-app.use('/api/signup', signupRouter);
+app.use("/api/signup", signupRouter);
+
+/*** Webpage routes below **********************************/
+// Serve the build
+app.use(express.static(__dirname + "/fgnb/build"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/fgnb/build/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
--- routes/example.js
+++ routes/example.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET expamle listing.
   use postman, send a get request to http://localhost/example
 */
-router.get('/', function(req, res, next) {
-  res.send("What's up motherfxxkers");
+router.get("/", function(req, res, next) {
+    res.send("What's up motherfxxkers");
 });
 
 module.exports = router;
