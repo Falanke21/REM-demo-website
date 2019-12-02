685ac5f046b9230c0810b6706bbc730d630829f2
Falanke21 <1065314944@qq.com>
2019-12-01 19:18:12 -0500

    add some more routes in biddings

--- routes/bidding.js
+++ routes/bidding.js
@@ -10,6 +10,49 @@ const { Item } = require("../models/item");
 const { Bidding } = require("../models/bidding");
 
 /* 
+    GET all biddings of a user.
+*/
+router.get("/", function(req, res, next) {
+    const userId = req.session.user || req.body.userId;
+    User.findById(userId)
+        .then(user => {
+            if (user === null) {
+                res.status(404).send({
+                    flag: false,
+                    error: "Can't find this user"
+                });
+            } else {
+                Bidding.find({ _id: { $in: user.biddings } }).then(biddings => {
+                    res.send({ flag: true, biddings: biddings });
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ flag: false, error: err });
+        });
+});
+
+/* 
+    GET all biddings to an item.
+*/
+router.get("/item", function(req, res, next) {
+    const itemId = req.body.itemId;
+    Item.findById(itemId)
+        .then(item => {
+            if (item === null) {
+                res.status(404).send({ flag: false, error: "can't find item" });
+            } else {
+                Bidding.find({ _id: { $in: item.biddings } }).then(biddings => {
+                    res.send({ flag: true, biddings: biddings });
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ flag: false, error: err });
+        });
+});
+
+/* 
     POST a bidding to an item.
 */
 router.post("/", function(req, res, next) {
@@ -30,6 +73,12 @@ router.post("/", function(req, res, next) {
 
             item.biddings.push(bidding._id);
             item.save();
+
+            User.findById(userId, (_, user) => {
+                user.biddings.push(bidding._id);
+                user.save();
+            });
+
             bidding
                 .save()
                 .then(result => {
@@ -40,6 +89,28 @@ router.post("/", function(req, res, next) {
                 });
         });
     }
+});
+
+/* 
+    PATCH a bidding to decline it.
+*/
+router.patch("/decline", function(req, res, next) {
+    const biddingId = req.body.biddingId;
+
+    Bidding.findById(biddingId)
+        .then(bidding => {
+            if (bidding === null) {
+                res.status(404).send({ flag: false });
+            } else {
+                bidding.accepted = false;
+                bidding.save().then(result => {
+                    res.send({ flag: true, bidding: result });
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ flag: false, error: err });
+        });
 });
 
 module.exports = router;
