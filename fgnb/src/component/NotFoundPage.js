0dc6e315d007349865cbbafbd98dde02aef874f6
zuoqiwx <zuoqiwchris@gmail.com>
2019-12-03 05:50:47 -0500

    Fixed logging out from profile page alerting error

--- fgnb/src/component/NotFoundPage.js
+++ fgnb/src/component/NotFoundPage.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class PageNotFound extends React.Component {
+    constructor(props) {
+        super(props);
+        setTimeout(() => {
+            this.props.history.push("/");
+        }, 1000);
+    }
     render() {
         return (
             <div>
                 Page Not Found :(
+                <div>
+                    Redirecting in 1 sec...
+                </div>
             </div>
         );
     }
