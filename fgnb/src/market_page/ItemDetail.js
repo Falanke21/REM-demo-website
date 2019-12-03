2f163b8795f8a79fe58dadb6eee421a856c29aca
Falanke21 <1065314944@qq.com>
2019-12-02 23:16:07 -0500

    add fetch call in itemDetail

--- fgnb/src/market_page/ItemDetail.js
+++ fgnb/src/market_page/ItemDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Navigation from "../navigation/Navigation";
 import ItemCard from "./ItemCard";
@@ -7,14 +7,38 @@ import BiddingDialog from "./BiddingDialog";
 import CurrentBiddings from "./CurrentBiddings";
 
 export default function ItemDetail({ match }) {
-    const item = tileData.find(x => match.params.itemId === x.id);
+    const [item, setItem] = useState({});
+    // const item = tileData.find(x => match.params.itemId === x.id);
+    useEffect(() => {
+        fetch("http://localhost:3001/api/item", {
+            method: "GET",
+            body: JSON.stringify({
+                itemId: match.params.itemId
+            })
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    Promise.reject(
+                        "Internal Server Errorrrrrrrrrrrrrrrrrrrrrrr"
+                    );
+                }
+            })
+            .then(json => {
+                setItem(json.item);
+            })
+            .catch(err => {
+                alert(err);
+            });
+    }, []);
 
     return (
         <div>
             <Navigation />
             <ItemCard item={item} />
-            <BiddingDialog itemId={item.id}/>
-            <CurrentBiddings item={item}/>
+            <BiddingDialog itemId={item._id} />
+            <CurrentBiddings item={item} />
         </div>
     );
 }
--- routes/item.js
+++ routes/item.js
@@ -11,7 +11,7 @@ const { authenticateAdmin } = require("../middlewares");
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
-        callback(null, "images");
+        callback(null, "public/images");
     },
     filename: function(req, file, callback) {
         // TODO: ADD MORE FILE TYPE HERE !!!!!
@@ -62,7 +62,7 @@ router.post("/", function(req, res, next) {
             });
 
         const item = new Item({
-            img: req.file.destination + "/" + req.file.filename,
+            img: "images/" + req.file.filename,
             title: title,
             seller: seller,
             price: price,
@@ -88,13 +88,24 @@ router.post("/", function(req, res, next) {
     GET all items that are available in the market.
 */
 router.get("/", function(req, res, next) {
-    Item.find({ inMarket: true })
+    const itemId = req.body.itemId;
+    if (itemId === undefined) {
+        Item.find({ inMarket: true })
         .then(result => {
             res.send({ flag: true, items: result });
         })
         .catch(err => {
             res.status(500).send();
         });
+    } else {
+        Item.find({ inMarket: true, _id: itemId })
+        .then(result => {
+            res.send({ flag: true, item: result });
+        })
+        .catch(err => {
+            res.status(500).send();
+        });
+    }
 });
 
 /*
