25340e874c1cbd95dc0af78524935fcd5ef90c9f
chriszhang <chris19980401@gmail.com>
2019-12-03 21:03:24 -0500

    final changed

--- fgnb/src/profile_page/Contact.js
+++ fgnb/src/profile_page/Contact.js
@@ -9,68 +9,8 @@ import { getState } from "statezero";
 //hard coded biddingId, need to be parsed everytime when we press the contact now 
 
 
-
-let getfetched = {
-    flag: true,
-    result: {
-        transaction: {
-            time: "2019-12-03T05:58:30.800Z",
-            _id: "5de5f98680cd8d3228eef214",
-            bidding: "5de5f0c18c9aee1ac8e12e6f",
-            finalPrice: 80,
-            __v: 0
-        },
-        bidding: {
-            accepted: true,
-            time: "2019-12-03T05:20:58.459Z",
-            _id: "5de5f0c18c9aee1ac8e12e6f",
-            item: "5de5f03cdf2fbf255cea6ef2",
-            buyer: "5de5f014df2fbf255cea6ef0",
-            seller: "5de5f014df2fbf255cea6ef0",
-            amount: 80,
-            __v: 0
-        },
-        item: {
-            inMarket: false,
-            submittedWhen: "2019-12-03T05:17:22.670Z",
-            biddings: ["5de5f089df2fbf255cea6ef3", "5de5f0c18c9aee1ac8e12e6f"],
-            _id: "5de5f03cdf2fbf255cea6ef2",
-            img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-black-select-2019?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1566956144418",
-            title: "Iphone11",
-            seller: "5de5f014df2fbf255cea6ef0",
-            price: 2000,
-            description: "wat",
-            location: "bay/college",
-            __v: 2
-        },
-        seller: {
-            phone: 1234459888,
-            profilePicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu4JAn8Ri7J-7gD3JqlRJt-YxfWqQCcXO_JE4AA_zrsgHKRUQA&s",
-            blacklisted: false,
-            sellings: ["5de5f030df2fbf255cea6ef1", "5de5f03cdf2fbf255cea6ef2"],
-            biddings: ["5de5f0c18c9aee1ac8e12e6f"],
-            _id: "5de5f014df2fbf255cea6ef0",
-            username: "Yaoshui21",
-            email: "chrissuper@gmail.com",
-            __v: 3
-        },
-        buyer: {
-            phone: null,
-            profilePicture: null,
-            blacklisted: false,
-            sellings: ["5de5f030df2fbf255cea6ef1", "5de5f03cdf2fbf255cea6ef2"],
-            biddings: ["5de5f0c18c9aee1ac8e12e6f"],
-            _id: "5de5f014df2fbf255cea6ef0",
-            username: "Falanke21",
-            email: "frankhua21@gmail.com",
-            __v: 3
-        }
-    }
-}
-
-
-function GetSellerPic(){
-    let sellerPic = getfetched.result.seller.profilePicture
+function GetSellerPic(props){
+    let sellerPic = props.sellerPic
     if(sellerPic == null){
         sellerPic = "https://previews.123rf.com/images/salamatik/salamatik1801/salamatik180100019/92979836-profile-anonymous-face-icon-gray-silhouette-person-male-default-avatar-photo-placeholder-isolated-on.jpg"
     }
@@ -82,8 +22,8 @@ function GetSellerPic(){
         ></img>)
 }
 
-function GetBuyerPic(){
-    let sellerPic = getfetched.result.buyer.profilePicture
+function GetBuyerPic(props){
+    let sellerPic = props.buyerPic
     if(sellerPic == null){
         sellerPic = "https://previews.123rf.com/images/salamatik/salamatik1801/salamatik180100019/92979836-profile-anonymous-face-icon-gray-silhouette-person-male-default-avatar-photo-placeholder-isolated-on.jpg"    }
     return(
@@ -94,8 +34,8 @@ function GetBuyerPic(){
         ></img>)
 }
 
-function GetSellerPhone(){
-    let sellerPhone = getfetched.result.seller.phone
+function GetSellerPhone(props){
+    let sellerPhone = props.sellerPhone
     if(sellerPhone == null){
         sellerPhone = "TBD"
     }
@@ -103,8 +43,8 @@ function GetSellerPhone(){
         sellerPhone)
 }
 
-function GetBuyerPhone(){
-    let buyerPhone = getfetched.result.buyer.phone
+function GetBuyerPhone(props){
+    let buyerPhone = props.buyerPhone
     if(buyerPhone == null){
         buyerPhone = "TBD"
     }
@@ -112,19 +52,20 @@ function GetBuyerPhone(){
         buyerPhone)
 }
 
-function UserBoxSeller() {
+function UserBoxSeller(props) {
+    console.log("In user seller box" + props);
     return (
         <div className="profile_box_container_Seller">
             <div className="profile_box">
                 <div className="userIconContainer">
-                    <GetSellerPic></GetSellerPic>
+                    <GetSellerPic sellerPic={props.seller.img}></GetSellerPic>
                     <div className="userNameContainer">
                         <div className="userBoxName">
                             {" "}
                             Name:
                             <div className="userInput">
                                 {" "}
-                                {getfetched.result.seller.username}
+                                {props.seller.username}
                             </div>
                         </div>
 
@@ -132,7 +73,7 @@ function UserBoxSeller() {
                             Email:
                             <div className="userInput">
                                 {" "}
-                                {getfetched.result.seller.email}
+                                {props.seller.email}
                             </div>
                         </div>
 
@@ -140,8 +81,7 @@ function UserBoxSeller() {
                             PhoneNum:
                             <div className="userInput">
                                 {" "}
-                                {/* {getfetched.result.seller.phone} */}
-                                <GetSellerPhone></GetSellerPhone>
+                                <GetSellerPhone sellerPhone={props.seller.sellerPhone}></GetSellerPhone>
                             </div>
                         </div>
                     </div>
@@ -150,19 +90,19 @@ function UserBoxSeller() {
         </div>
     );
 }
-function UserBoxBuyer() {
+function UserBoxBuyer(props) {
     return (
         <div className="profile_box_container_Buyer">
             <div className="profile_box">
                 <div className="userIconContainer">
-                    <GetBuyerPic></GetBuyerPic>
+                    <GetBuyerPic buyerPic={props.img} ></GetBuyerPic>
                     <div className="userNameContainer">
                         <div className="userBoxName">
                             {" "}
                             Name:
                             <div className="userInput">
                                 {" "}
-                                {getfetched.result.buyer.username}
+                                {props.buyer.username}
                             </div>
                         </div>
 
@@ -170,7 +110,7 @@ function UserBoxBuyer() {
                             Email:
                             <div className="userInput">
                                 {" "}
-                                {getfetched.result.buyer.email}
+                                {props.buyer.email}
                             </div>
                         </div>
 
@@ -178,8 +118,8 @@ function UserBoxBuyer() {
                             PhoneNum:
                             <div className="userInput">
                                 {" "}
-                                {/* {getfetched.result.buyer.phone} */}
-                                <GetBuyerPhone></GetBuyerPhone>
+                                {props.phone}
+                                <GetBuyerPhone buyerPhone={props.buyer.phone}></GetBuyerPhone>
                             </div>
                         </div>
                     </div>
@@ -190,13 +130,18 @@ function UserBoxBuyer() {
 }
 
 class Contact extends React.Component {
-    state = {
-        currentState: this.props.match.params.transactionId,
-        transaction:{},
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentState: this.props.match.params.transactionId,
+            transaction: {seller: {username:"",email:"", img:"",phone:""}, buyer: {username:"",email:"", img:"",phone:""},item:{img:""}},
+        }
+        this._getData();
     }
 
-    componentWillMount() {
-        const id = this.state.currentState
+
+    _getData() {
+        const id = this.props.match.params.transactionId
         console.log(id)
         const url = `http://localhost:3001/api/transaction/${id}`;
         fetch(url)
@@ -211,8 +156,9 @@ class Contact extends React.Component {
             .then(json => {
                 console.log(json)
                 if (json.flag) {
+                    console.log("entered flag");
                     this.setState({
-                        transaction: json
+                        transaction: json.result
                     });
 
                 }
@@ -220,10 +166,10 @@ class Contact extends React.Component {
             .catch(error => {
                 console.log(error);
             })
-        getfetched = this.transaction
     }
 
     render() {
+        console.log("In render: " + this.state.transaction);
         return (
             <div>
                 <div>
@@ -234,13 +180,13 @@ class Contact extends React.Component {
                     <div className="Contact_box_text_seller">Seller</div>
                     <div className="Contact_box_text_buyer">Buyer</div>
                 </div>
-                <UserBoxSeller></UserBoxSeller>
+                <UserBoxSeller seller={this.state.transaction.seller}></UserBoxSeller>
                 <div className="Contact_congrads_box">
                     <div className="Contact_congrads_box_text">
                         congratulation!
                     </div>
                     <div className="Contact_congrads_box_pic">
-                        <img src={getfetched.result.item.img} className="bounding-box"></img>
+                        <img src={this.state.transaction.item.img} className="bounding-box"></img>
                         <img
                             src="http://s.aigei.com/src/img/png/13/133d8084d3ba4d01922365fb631f415b.png?imageMogr2/auto-orient/thumbnail/!52x152r/gravity/Center/crop/52x152/quality/85/&e=1735488000&token=P7S2Xpzfz11vAkASLTkfHN7Fw-oOZBecqeJaxypL:bIfkc9DQrdr629xHIAFmGXWR5vs=
 "
@@ -252,7 +198,7 @@ class Contact extends React.Component {
                         ></img>
                     </div>
                 </div>
-                <UserBoxBuyer></UserBoxBuyer>
+                <UserBoxBuyer buyer={this.state.transaction.buyer}></UserBoxBuyer>
             </div>
         );
     }
